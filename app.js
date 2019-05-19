9dd7fd4d7837376ed81ea2e345e11d2c4d748e33
piermarcobarbe <barbep@usi.ch>
2019-05-19 12:29:37 +0200

    Added transmission web ui, fs module

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 var Client = require('node-torrent');
 var client = new Client({logLevel: 'DEBUG'});
+var express = require('express');
+var app = express();
+var path = require('path');
+var fs = require('fs');
+
+console.log(path.join(__dirname, "public/"))
+app.use('/', express.static(path.join(__dirname, "public/")));
+//
+// app.post('/rpc', function (req, res) {
+//     console.log(req.headers);
+//     console.log(req.body);
+//
+//     console.log("-------------------");
+//
+//     res.sendStatus(200);
+//     res.end();
+// });
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
